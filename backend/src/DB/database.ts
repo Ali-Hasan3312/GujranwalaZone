88a5ed9b63881ff5fffe7bd15d114a4cba359684
Ali-Hasan3312 <alihasan331229@gmail.com>
2024-06-15 15:58:36 +0500

    add mongodb atlas url

--- backend/src/DB/database.ts
+++ backend/src/DB/database.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-export const connectDB = ()=>{
-    mongoose.connect(`${process.env.MONGODB_URL}`,{
+export const connectDB = async()=>{
+  await  mongoose.connect(`${process.env.MONGODB_URL}`,{
         dbName: "Gujranwala_Zone"
     })
     .then((c)=> console.log(`MongoDB Connected to ${c.connection.host}`))
